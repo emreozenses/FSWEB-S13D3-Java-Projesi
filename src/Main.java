633b5c01abc83631b7fa6919e4d400289d90fc3c
Emre OZENSES <emreozenses@gmail.com>
2023-11-15 16:44:16 +0300

    person task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Person person1 = new Person("Emre","Özenses",39,"male","istanbul","dev");
+
+        System.out.println("İsim:"+person1.getFirstName()+" Soyisim:"+person1.getLastName()+" Yaş:"+person1.getAge());
+        System.out.println("Teen mi?:"+person1.isTeen());
+        System.out.println(person1);
     }
+
+
 }
---
+++ src/Person.java
@@ -0,0 +1,59 @@
+public class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+    String gender;
+    String homeTown;
+    String job;
+
+
+
+    public Person(String firstName,String lastName,int age){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+     }
+
+    public Person(String firstName,String lastName,int age,String gender,String homeTown,String job){
+    this(firstName, lastName, age);
+        this.gender=gender;
+        this.homeTown=homeTown;
+        this.job=job;
+    }
+
+
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+
+    }
+
+    public  int getAge(){
+        return age;
+    }
+
+    public boolean isTeen(){
+
+        return 13 <= age && age < 19;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", homeTown='" + homeTown + '\'' +
+                ", job='" + job + '\'' +
+                '}';
+    }
+
+}
